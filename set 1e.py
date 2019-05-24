567f83a1c100c308b92d4316da761f3d508e6409
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-05-24 11:35:11 +0530

    Add files via upload

---
+++ set 1e.py
@@ -0,0 +1,5 @@
+#anju
+roman=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+integer=input()
+if integer in roman:
+    print(roman.index(integer)+1)
