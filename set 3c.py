fadd1a2971843c5cf12cd63bcd062e96e093feb5
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-06-09 13:35:27 +0530

    Add files via upload

---
+++ set 3c.py
@@ -0,0 +1,9 @@
+a1,b1=map(int,input().split())
+input()
+Num=list(map(int,input().split()))
+k=list(map(int,input().split()))
+s=''
+for i in k:
+  Num.append(i)
+  s+=str(max(Num))+' '
+print(s[:-1])
